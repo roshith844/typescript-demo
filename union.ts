7e2f97c7dc806d46f7b630d6c0192fecda0b2791
Roshith P <roshith844@gmail.com>
2023-01-02 19:40:22 +0530

    example : union

---
+++ union.ts
@@ -0,0 +1,10 @@
+// Varriable
+let score : number | boolean = 43;
+score = false
+
+// Array 
+const data : (string | number)[] = ["something", 1]
+
+let orderStatus : 'pending' | 'shipped ' | 'delivered'
+ orderStatus = 'pending'
+// orderStatus = 'jkjk' cannot assign values which is not given
