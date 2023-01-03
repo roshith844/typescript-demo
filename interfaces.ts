1d9ace95df957faf9b90dc7325ec7fc4e7ee9e5d
Roshith P <roshith844@gmail.com>
2023-01-03 09:59:24 +0530

    interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,10 +1,18 @@
-interface User{
+interface User {
       name: string
       age?: number // Optional
-      id: number 
+      id: number
       email: string
+      generateUniqueId: () => string // Should be a string with return value of ype string
+     
+      
 }
-let user : User = {name: 'nazim', id: 1, email: "example@example.com"}
+// can add values without changing first definition
+interface User{
+      anotherFunction(): string
+}
+let user: User = { name: 'nazim', id: 1, email: "example@example.com", generateUniqueId: () => "string",
+anotherFunction:  function (){ return "something"} }
 export interface Accountant extends User {
 
 }
